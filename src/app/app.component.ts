ac4bfdfc489a60888e3072d56e95b2674ff51044
Peterson <44592787+petscaramussi@users.noreply.github.com>
2024-02-28 18:04:41 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DateValidator } from './validators/dateValidator';
+import { GenericValidator } from './validators/cpfValidator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form';
+
+export class AppComponent implements OnInit{
+
+  registerForm: FormGroup;
+  finalValues: JSON;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+
+    this.registerForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      dataNasc: new FormControl('', [Validators.required, DateValidator.ptDate]),
+      cpf: new FormControl('', [Validators.required, GenericValidator.isValidCpf()]),
+      endereco: new FormControl('', Validators.required),
+      sexo: new FormControl('', Validators.required),
+      }
+    )
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+    this.finalValues = this.registerForm.value;
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+    
   ],
-  providers: [],
+  providers: [provideNgxMask()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
